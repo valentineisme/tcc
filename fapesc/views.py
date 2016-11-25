55e253b617769c0c71e2000d3e7691133dad210f
FilipeRodrigues <filipejoaorodrigues1992@gmail.com>
2016-11-24 23:50:11 -0200

    Form Imagem bonitinho

--- fapesc/views.py
+++ fapesc/views.py
@@ -73,10 +73,6 @@ def FormImagem(request):
 
 
 @login_required
-def handle_uploaded_file(f):
-    with open('some/file/name.txt', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
 
 def CadImagem(request):
     if request.POST:
